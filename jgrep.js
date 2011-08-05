6db4f1b3e77a5076ba6b62ca12ba9b774933e0a7
Pieter Loubser <ploubser@gmail.com>
2011-08-05 17:49:56 +0100

    Updated site layout and style. Added some javascript.

---
+++ jgrep.js
@@ -0,0 +1,55 @@
+$(document).ready(function()
+{
+    var urlitems=window.location.href.split("#");
+    var curpage=urlitems[1];
+    if(!(curpage==null)){
+        $.ajax({
+            url: curpage + ".html",
+            cache: false,
+            success: function(html){
+                $("#container").replaceWith(html);
+            }
+        });
+    }
+    else{
+        $.ajax({
+            url: "home.html",
+            cache: false,
+            success: function(html){
+                $("#container").replaceWith(html);
+            }
+        });
+    }
+
+    $(".menu-item").hover(function(e)
+    {
+        $(e.currentTarget).css("background-color","#434343");
+        $(this).css('cursor', 'pointer');
+    });
+
+    $(".menu-item").mouseout(function(e)
+    {
+        $(e.currentTarget).css("background-color", "#000000");
+    });
+
+    $(".menu-item").click(function(e)
+    {
+        if(e.currentTarget.id == "menu-documentation"){
+            $(location).attr("href", "http://rubydoc.info/gems/jgrep/1.2.1/frames")
+        }
+        else if(e.currentTarget.id == "menu-issues"){
+            $(location).attr("href", "https://github.com/psy1337/JSON-Grep/issues")
+        }
+        else{
+            $.ajax({
+                url: e.currentTarget.id.replace("menu-", "") + ".html",
+                cache: false,
+                success: function(html){
+                    $("#container").replaceWith(html);
+                    window.location.href = "#" + e.currentTarget.id.replace("menu-", "");
+
+                }
+            });
+        }
+    });
+});
