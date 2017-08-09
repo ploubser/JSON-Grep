f88da5dcc3ed36738d5702b7843db761fb796aad
R.I.Pienaar <rip@devco.net>
2017-08-09 16:38:48 +0200

    Rubocop for specs

--- spec/unit/scanner_spec.rb
+++ spec/unit/scanner_spec.rb
@@ -1,11 +1,8 @@
-#! /usr/bin/env ruby
-
 require File.dirname(__FILE__) + "/../spec_helper"
-
 
 module JGrep
   describe Scanner do
-    describe '#get_token' do
+    describe "#get_token" do
       it "should identify a '(' token" do
         scanner = Scanner.new("(")
         token = scanner.get_token
@@ -21,38 +18,37 @@ module JGrep
       it "should identify an 'and' token" do
         scanner = Scanner.new("and ")
         token = scanner.get_token
-        expect(token).to eq(["and", "and"])
+        expect(token).to eq(%w[and and])
       end
 
       it "should identify a '&&' token" do
         scanner = Scanner.new("&& ")
         token = scanner.get_token
-        expect(token).to eq(["and", "and"])
+        expect(token).to eq(%w[and and])
       end
 
       it "should identify an 'or' token" do
         scanner = Scanner.new("or ")
         token = scanner.get_token
-        expect(token).to eq(["or", "or"])
+        expect(token).to eq(%w[or or])
       end
 
-      it "should identify a "||" token" do
+      it "should identify a " || " token" do
         scanner = Scanner.new("|| ")
         token = scanner.get_token
-        expect(token).to eq(["or", "or"])
-
+        expect(token).to eq(%w[or or])
       end
 
       it "should identify an 'not' token" do
         scanner = Scanner.new("not ")
         token = scanner.get_token
-        expect(token).to eq(["not", "not"])
+        expect(token).to eq(%w[not not])
       end
 
       it "should identify an '!' token" do
         scanner = Scanner.new("!")
         token = scanner.get_token
-        expect(token).to eq(["not", "not"])
+        expect(token).to eq(%w[not not])
       end
 
       it "should identify a statement token" do
@@ -64,21 +60,21 @@ module JGrep
       it "should identify a complex array statement" do
         scanner = Scanner.new("[foo=bar and bar=foo]")
         token = scanner.get_token
-        expect(token).to eq(["statement", [["statement", "foo=bar"], ["and", "and"], ["statement", "bar=foo"]]])
+        expect(token).to eq(["statement", [["statement", "foo=bar"], %w[and and], ["statement", "bar=foo"]]])
       end
 
       it "should fail if expression terminates with 'and'" do
         scanner = Scanner.new("and")
 
-        expect {
-          token = scanner.get_token
-        }.to raise_error("Class name cannot be 'and', 'or', 'not'. Found 'and'")
+        expect do
+          scanner.get_token
+        end.to raise_error("Class name cannot be 'and', 'or', 'not'. Found 'and'")
       end
 
       it "should identify a '+' token" do
         scanner = Scanner.new("+foo")
         token = scanner.get_token
-        expect(token).to eq(["+","foo"])
+        expect(token).to eq(["+", "foo"])
       end
 
       it "should identify a '-' token" do
